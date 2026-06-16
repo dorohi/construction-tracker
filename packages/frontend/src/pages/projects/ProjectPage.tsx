2206d7247e5d07330f75c59dd35b62f126b4edce
m.m.dorohi <m.m.dorohi@gmail.com>
2026-06-16 11:12:04 +0300

    Страница проекта: убрал копейки во всех денежных значениях (бюджет, потрачено, запланировано, остаток, материалы/работы/инструменты/доставки)

--- packages/frontend/src/pages/projects/ProjectPage.tsx
+++ packages/frontend/src/pages/projects/ProjectPage.tsx
@@ -44,6 +44,7 @@ function formatCurrency(amount: number) {
   return new Intl.NumberFormat("ru-RU", {
     style: "currency",
     currency: "RUB",
+    maximumFractionDigits: 0,
   }).format(amount);
 }
 
